482fe3e3c8326b154ff4becb53ef59eecd02a4c2
quintin tuck <tuckquintin@hotmail.com>
2025-01-16 11:03:48 -0330

    changes to hello world app. Attempted connect to mongo db, needs fixes from paul to continue.

--- HelloWorldExpress/app.js
+++ HelloWorldExpress/app.js
@@ -1,15 +1,27 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send(`Hello World!`)
+  res.send(`Hello World!`);
 })
 
-app.get('/hello/:id', (req, res)=> {
-    res.send(`${req.params.id}`)
+app.get('/hello/:name', (req, res)=> {
+    res.send(`Hello ${req.params.name}!`);
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+app.put(`/put/:message`, (req, res) =>{
+    res.send(`you sent the message: ${req.params.message}`);
 })
+
+mongoose.connect('mongodb://Student17:Student17@logan', { dbName: 'home17' })
+    .then(() => {
+        console.log("Connected to the database!");
+        app.listen(port, () => {
+            console.log(`Example app listening on port ${port}`);
+        });
+    })
+    .catch(() => {
+        console.log("Failed to connect to the database.");
+    });
